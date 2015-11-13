cce7c4acb2b8f9cc30281404c2d6453b11e74847
edT <edwardt.tril@gmail.com>
2015-11-12 16:04:59 -0800

    fix undef function

--- test/functional/pinfo_test.exs
+++ test/functional/pinfo_test.exs
@@ -12,43 +12,43 @@ defmodule ExRPC.Test.Functional.Pinfo do
   end
 
   test "Pinfo on living process on local node" do
-    pid = ExRPC.call(master, Process, :spawn, [fn -> :timer.sleep(100000) end])
+    pid = ExRPC.call(master, Kernel, :spawn, [fn -> :timer.sleep(100000) end])
     assert true = ExRPC.call(master, Process, :alive?, [pid])
     assert [] != ExRPC.pinfo(master, pid)
   end
 
   test "Pinfo on dead process on local node" do
-    pid = ExRPC.call(master, Process, :spawn, [fn -> Process.exit(self, :normal) end])
+    pid = ExRPC.call(master, Kernel, :spawn, [fn -> Process.exit(self, :normal) end])
     assert false = ExRPC.call(master, Process, :alive?, [pid])
     assert :undefined = ExRPC.pinfo(master, pid)
   end
 
   test "Pinfo status on living process on local node" do
-    pid = ExRPC.call(master, Process, :spawn, [fn -> :timer.sleep(100000) end])
+    pid = ExRPC.call(master, Kernel, :spawn, [fn -> :timer.sleep(100000) end])
     assert true = ExRPC.call(master, Process, :alive?, [pid])
     assert {:status,:waiting}!= ExRPC.pinfo(master, pid, [:status])
   end
 
   test "Pinfo on living process on slave node" do
-    pid = ExRPC.call(slave, Process, :spawn, [fn -> :timer.sleep(100000) end])
+    pid = ExRPC.call(slave, Kernel, :spawn, [fn -> :timer.sleep(100000) end])
     assert true = ExRPC.call(slave, Process, :alive?, [pid])
     assert [] != ExRPC.pinfo(slave, pid)
   end
 
   test "Pinfo on dead process on slave node" do
-    pid = ExRPC.call(slave, Process, :spawn, [fn -> Process.exit(self, :normal) end])
+    pid = ExRPC.call(slave, Kernel, :spawn, [fn -> Process.exit(self, :normal) end])
     assert false = ExRPC.call(slave, Process, :alive?, [pid])
     assert :undefined = ExRPC.pinfo(slave, pid)
   end
 
   test "Pinfo on process that throws on slave node" do
-    pid = ExRPC.call(slave, Process, :spawn, [fn -> throw(:xxxxxx) end])
+    pid = ExRPC.call(slave, Kernel, :spawn, [fn -> throw(:xxxxxx) end])
     assert false = ExRPC.call(slave, Process, :alive?, [pid])
     assert :undefined = ExRPC.pinfo(slave, pid)
   end
 
   test "Pinfo status on living process on slave node" do
-    pid = ExRPC.call(slave, Process, :spawn, [fn -> :timer.sleep(100000) end])
+    pid = ExRPC.call(slave, Kernel, :spawn, [fn -> :timer.sleep(100000) end])
     assert true = ExRPC.call(slave, Process, :alive?, [pid])
     assert {:status,:waiting}!= ExRPC.pinfo(slave, pid, [:status])
   end
